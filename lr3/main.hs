76a87661593a2cb479b82f54106485916d284bf0
ArsentiiDev <56321438+ArsentiiDev@users.noreply.github.com>
2021-11-17 13:25:23 +0200

    added tests

--- lr3/main.hs
+++ lr3/main.hs
@@ -1,6 +1,15 @@
 
+--Лабораторна робота №3
+--студента групи КН-32 підгрупа 1
+--Григор'єва Арсентія
+--Варіант 9
+
+--Мета:Набути досвiду визначення та використання функцiй вищого порядку.
+
+
 import Data.List
---Завдання 1 
+--Завдання 1: Вставити у список через кожнi n елементiв вказане значення, напр.
+--через n=2 значення ’z’: "1234590"⇒ "12z34z59z0".
 --а)
 insert :: Int -> a -> [a] -> [a]
 insert n y xs = countdown n xs where
@@ -14,7 +23,19 @@ insertAtN n y  = intercalate [y] . groups n
   where
     groups n = takeWhile (not.null) . unfoldr (Just . splitAt n)
 
---Завдання 2
+--Тести
+-- ghci> Main.insert 2 'z' "aaaaaaaaaaaa"
+-- "aazaazaazaazaazaaz"
+-- ghci>
+-- ghci> Main.insert 3 'a' "123456"      
+-- "123a456a"
+-- ghci>
+-- ghci> insertAtN 2 'b' "123456789"
+-- "12b34b56b78b9"
+-- ghci>
+
+
+--Завдання 2: Знайти перше просте число в указаному дiапазонi.
 --а)
 nPrimes x y =  head [z | z <- [x..y], isPrime z]
 isPrime :: (Integral a) => a -> Bool
@@ -31,8 +52,18 @@ nPrimes2 x y =  head [z | z <- [x..y], isPrime z]
 isPrime2 :: (Integral a) => a -> Bool
 isPrime2 k | k <=1 = False | otherwise = 0 `notElem` map (mod k)[2..k-1]
 
-
-
+--Тести
+-- ghci> nPrimes 5 25
+-- 5
+-- ghci> nPrimes 7 24
+-- 7
+-- ghci> nPrimes 6 13
+-- 7
+-- ghci> nPrimes2 6 13
+-- 7
+-- ghci> nPrimes2 17 45
+-- 17
+-- ghci>
 
 
 
