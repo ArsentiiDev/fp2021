5c9755113b476624edce85f0797adc09fe35abcd
ArsentiiDev <56321438+ArsentiiDev@users.noreply.github.com>
2021-11-24 10:18:07 +0200

    Added tests to lr4

--- lr4/lr4.hs
+++ lr4/lr4.hs
@@ -4,6 +4,7 @@
 --зва статтi, назва журналу, рiк, номер журналу, сторiнки) або тезами доповiдi
 --(автор/спiвавтори, назва доповiдi, назва конференцiї, мiсто, рiк, сторiнки). Ви-
 --значне функцiї для :
+--пошуку усiх видавництв, журналiв, конференцiй;
 
 
 
@@ -28,7 +29,6 @@ showPublish [] = []
 showPublish ( Book _ _ _ [] _:xs)  = showPublish xs 
 showPublish ( Book _ _ _ n _ :xs) = isAlreadyExists n (showPublish xs)
 
-
 showPublish ( Article _ _ [] _ _ _:xs) = showPublish xs
 showPublish ( Article _ _ n _ _ _:xs) = isAlreadyExists n (showPublish xs)
 
@@ -41,12 +41,44 @@ publications = [
     Thesus "Artem" "Metaphysics" "Conference of physics" "Amsterdam" 2020 12,
     Book "Andrii" "UI/UX design" "Rivne" "Rivne book" 2020,
     Book "Anton" "Study easy" "Kyiv" "Malyatko" 2021,
-    Article "Arsentii" "Web developing" "Webdev" 2021 30 25
-             ]
+    Article "Arsentii" "Web developing" "Webdev" 2021 30 25,
+    Book "Sergey" "Elder Rings" "Kherson" "Malyatko" 2001,
+    Book "Sergey" "Elder Rings" "Kherson" "Malyatko" 2001
+            ]
+
+--масив значень для тестування створеної функції
+
 
 isAlreadyExists n l
   | n `elem` l = l
   | otherwise = n:l
 
+--Функція іsAlreadyExists перевіряє чи зустрічається дане джерело у нашому списку
+--Якщо так - Пропускає його
+--Якщо ні - додає в результуючий масив
 
--- n:(Lancet) -- Malyatko,Lancet
+
+--Тестування
+--publications = [
+    -- Book "Sergey" "Elder Rings" "Kherson" "Malyatko" 2001,
+    -- Article "Anatolii" "Covid-19" "Lancet" 2021 12 250,
+    -- Thesus "Artem" "Metaphysics" "Conference of physics" "Amsterdam" 2020 12,
+    -- Book "Andrii" "UI/UX design" "Rivne" "Rivne book" 2020,
+    -- Book "Anton" "Study easy" "Kyiv" "Malyatko" 2021,
+    -- Article "Arsentii" "Web developing" "Webdev" 2021 30 25, 
+    --]
+--ghci> showPublish publications
+--["Lancet","Conference of physics","Rivne book","Malyatko","Webdev"]
+
+-- publications = [
+--     Book "Sergey" "Elder Rings" "Kherson" "Malyatko" 2001,
+--     Article "Anatolii" "Covid-19" "Lancet" 2021 12 250,
+--     Thesus "Artem" "Metaphysics" "Conference of physics" "Amsterdam" 2020 12,
+--     Book "Andrii" "UI/UX design" "Rivne" "Rivne book" 2020,
+--     Book "Anton" "Study easy" "Kyiv" "Malyatko" 2021,
+--     Article "Arsentii" "Web developing" "Webdev" 2021 30 25,
+--     Book "Sergey" "Elder Rings" "Kherson" "Malyatko" 2001,
+--     Book "Sergey" "Elder Rings" "Kherson" "Malyatko" 2001,
+--              ]
+--ghci> showPublish publications
+--["Lancet","Conference of physics","Rivne book","Webdev","Malyatko"]
