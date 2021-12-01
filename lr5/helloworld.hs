7f08be25151e090a0f0497d90cd955d9cd2a0d25
ArsentiiDev <56321438+ArsentiiDev@users.noreply.github.com>
2021-12-01 13:47:00 +0200

    finished 5lr

--- lr5/helloworld.hs
+++ lr5/helloworld.hs
@@ -1,4 +1,46 @@
-main = do 
-        puStrLn "Hello, what's your name"
-        name <- getLine
-        puStrLn ("Hey " ++ name ++ ", you rock")
+import System.IO (IOMode (ReadMode), openFile, hIsEOF, hGetLine, hClose)
+import Distribution.Compat.CharParsing (CharParsing(text))
+import qualified Data.Functor
+
+--Завдання 1: Вставити у список через кожнi n елементiв вказане значення, напр.
+--через n=2 значення ’z’: "1234590"⇒ "12z34z59z0".
+--а)
+
+prompt :: (Read a) => String -> IO a
+prompt s = (putStr s >> getLine) Data.Functor.<&> read
+
+--insert :: Int -> a -> [a] -> [a]
+insert n y xs = countdown n xs where
+   countdown 0 xs = y:countdown n xs -- reset to original n
+   countdown _ [] = []
+   countdown m (x:xs) = x:countdown (m-1) xs
+
+
+main :: IO ()
+main = do
+
+-- Закоментуйте, будь ласка, один варіант виводу та один варіант вводу для коректної роботи програми
+
+-- -- З клавіатури
+    n <- prompt "The number? "
+    c <- prompt "The char?"
+    l <- prompt "The line? "
+
+-- -- На екран
+    let area = insert n c l
+    putStrLn $ "The area of that triangle is " ++ area
+
+-- -- З файлу
+--     handle <- openFile "input.txt" ReadMode 
+--     l <- hGetLine handle
+--     str <- hGetLine handle
+--     let n = read str :: Int
+--     hClose handle
+
+-- -- В файл
+--     let res = divideListN l n
+--     let f = fst res
+--     let s = snd res
+--     let res_final = f ++ "\n" ++ s
+--     writeFile "output.txt" res_final
+--     putStr "Result is in the file output.txt :) "
